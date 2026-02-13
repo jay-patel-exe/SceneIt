8ba0c74e4155b4b1d276db41447ed172c49e6937
Jay <jaypatel100507@gmail.com>
2026-02-13 22:30:14 +0530

    utils done

--- src/index.js
+++ src/index.js
@@ -7,3 +7,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Hello ${process.env.PORT}`)
+    });
+})
+.catch((err) => {
+    console.log("MongoDB connection error !! ", err);
+})
