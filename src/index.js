704612ce2bf2f8a0d27539cc4db91dc5e2b7b023
Jay <jaypatel100507@gmail.com>
2026-02-14 17:27:03 +0530

    project setup done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose"
+import { app } from './app.js'
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 
 dotenv.config({
-    path : './env'
+    path: './.env'
 })
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Hello ${process.env.PORT}`)
-    });
-})
-.catch((err) => {
-    console.log("MongoDB connection error !! ", err);
-})
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Hello ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error !! ", err);
+    })
--- src/models/user.js
+++ src/models/user.js
@@ -49,7 +49,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next()
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -66,18 +66,18 @@ userSchema.methods.generateAccessToken = function () {
     },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn: ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 }
 
-userSchema.methods.refreshAccessToken = function () { 
+userSchema.methods.generateRefreshToken = function () { 
     return jwt.sign({
         _id: this._id,
     },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: REFRESH_TOKEN_EXPIRY
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 }
